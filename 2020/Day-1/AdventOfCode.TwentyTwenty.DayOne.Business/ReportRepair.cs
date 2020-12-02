8262c734896b4c309eea9b86a50cf8c5605a1dd9
shareitcode <45316299+shareitcode@users.noreply.github.com>
2020-12-02 19:25:13 +0100

    [2020] [DAY 1] Organize solution

---
+++ 2020/Day-1/AdventOfCode.TwentyTwenty.DayOne.Business/ReportRepair.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+
+namespace AdventOfCode.TwentyTwenty.DayOne.Business
+{
+	public sealed class ReportRepair
+	{
+		public static int SumWithTwoNumbersFromNumbers(short[] numbers)
+		{
+			int result = 0;
+			foreach (short nOne in numbers)
+				foreach (short nTwo in numbers.Where(nTwo => nOne + nTwo == 2020))
+				{
+					result = nOne * nTwo;
+					break;
+				}
+			return result;
+		}
+
+		public static int SumWithThreeNumbersFromNumbers(short[] numbers)
+		{
+			int result = 0;
+			foreach (short nOne in numbers)
+				foreach (short nTwo in numbers)
+					foreach (short nThree in numbers.Where(nThree => nOne + nTwo + nThree == 2020))
+					{
+						result = nOne * nTwo * nThree;
+						break;
+					}
+			return result;
+		}
+	}
+}
