865776f3b8ffa118077e380af6607b3ba7f3d175
shareitcode <45316299+shareitcode@users.noreply.github.com>
2020-12-04 17:23:08 +0100

    [2020] [DAY 2] [PART 1] WIP business rules implemented

--- 2020/Day-2/AdventOfCode.TwentyTwenty.DayTwo/AdventOfCode.TwentyTwenty.DayTwo.Business/PasswordPhilosophy.cs
+++ 2020/Day-2/AdventOfCode.TwentyTwenty.DayTwo/AdventOfCode.TwentyTwenty.DayTwo.Business/PasswordPhilosophy.cs
@@ -1,14 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode.TwentyTwenty.DayTwo.Business
 {
-	public sealed class PasswordPhilosophy
-	{
-		public static string GetNumbersOfValidPasswords(string[] passwords)
-		{
-			return string.Empty;
-		}
-	}
+    public sealed class PasswordPhilosophy
+    {
+        const string leastTimeOccurrenceNumberRegex = "^[0-9]{1,2}-";
+        const string mostTimeOccurrenceNumberRegex = "-[0-9]{1,2}";
+        const string charOccurrenceRegex = " [a-z]:";
+        const string passwordRegex = ": [a-z]+";
+
+        public static PasswordPolicy GetPasswordPolicy(string passwordWithPolicy)
+        {
+            return new PasswordPolicy()
+            {
+                NumberLeastTimeCharOccurrence = GetNumberLeastTimeCharOccurrence(passwordWithPolicy),
+                NumberMostTimeCharOccurrence = GetNUmberMostTimeCharOccurrence(passwordWithPolicy),
+                OccurrenceCharacterReference = GetOccurrenceCharacterReference(passwordWithPolicy)
+            };
+        }
+
+        public static short GetNumberLeastTimeCharOccurrence(string passwordWithPolicy)
+        {
+            string regexResult = Regex.Match(passwordWithPolicy, leastTimeOccurrenceNumberRegex).Value;
+            string number = regexResult.Replace("-", string.Empty);
+            short.TryParse(number, out short numerParsed);
+            return numerParsed;
+        }
+
+        public static short GetNUmberMostTimeCharOccurrence(string passwordWithPolicy)
+        {
+            string regexResult = Regex.Match(passwordWithPolicy, mostTimeOccurrenceNumberRegex).Value;
+            string number = regexResult.Replace("-", string.Empty);
+            short.TryParse(number, out short numerParsed);
+            return numerParsed;
+        }
+
+        public static char GetOccurrenceCharacterReference(string passwordWithPolicy)
+        {
+            string regexResult = Regex.Match(passwordWithPolicy, charOccurrenceRegex).Value;
+			string referenceCharacter = regexResult.Replace(":", string.Empty).Trim();
+            char.TryParse(referenceCharacter, out char referenceCharacterParsed);
+            return referenceCharacterParsed;
+        }
+
+        public static string GetPassword(string passwordWithPolicy)
+        {
+            string regexResult = Regex.Match(passwordWithPolicy, passwordRegex).Value;
+            return regexResult.Replace(":", string.Empty).Trim();
+        }
+
+        public static bool PasswordIsValid(PasswordPolicy passwordPolicy, string password)
+        {
+            var regexResult = Regex.Matches(password, passwordPolicy.OccurrenceCharacterReference.ToString());
+            return regexResult.Count >= passwordPolicy.NumberLeastTimeCharOccurrence
+                && regexResult.Count <= passwordPolicy.NumberMostTimeCharOccurrence;
+        }
+
+        public static string GetNumbersOfValidPasswords(string[] passwords)
+        {
+            return string.Empty;
+        }
+    }
+
+    public sealed class PasswordPolicy
+    {
+        public short NumberLeastTimeCharOccurrence { get; set; }
+        public short NumberMostTimeCharOccurrence { get; set; }
+        public char OccurrenceCharacterReference { get; set; }
+    }
 }
--- 2020/Day-2/AdventOfCode.TwentyTwenty.DayTwo/AdventOfCode.TwentyTwenty.DayTwo.Tests/PasswordPhilosophyTests.cs
+++ 2020/Day-2/AdventOfCode.TwentyTwenty.DayTwo/AdventOfCode.TwentyTwenty.DayTwo.Tests/PasswordPhilosophyTests.cs
@@ -1,3 +1,4 @@
+using AdventOfCode.TwentyTwenty.DayTwo.Business;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AdventOfCode.TwentyTwenty.DayTwo.Tests
@@ -6,8 +7,48 @@ namespace AdventOfCode.TwentyTwenty.DayTwo.Tests
 	public class PasswordPhilosophyTests
 	{
 		[TestMethod]
-		public void TestMethod1()
+		public void TryBusinessRulesWithExampleOne()
 		{
+			string passwordsWithPolicy = "1-3 a: abcde";
+			PasswordPolicy passwordPolicy = PasswordPhilosophy.GetPasswordPolicy(passwordsWithPolicy);
+			string password = PasswordPhilosophy.GetPassword(passwordsWithPolicy);
+			bool passwordIsValid = PasswordPhilosophy.PasswordIsValid(passwordPolicy, password);
+
+			Assert.AreEqual(1, passwordPolicy.NumberLeastTimeCharOccurrence);
+			Assert.AreEqual(3, passwordPolicy.NumberMostTimeCharOccurrence);
+			Assert.AreEqual('a', passwordPolicy.OccurrenceCharacterReference);
+			Assert.AreEqual("abcde", password);
+			Assert.IsTrue(passwordIsValid);
+		}
+
+		[TestMethod]
+		public void TryBusinessRulesWithExampleTwo()
+		{
+			string passwordsWithPolicy = "1-3 b: cdefg";
+			PasswordPolicy passwordPolicy = PasswordPhilosophy.GetPasswordPolicy(passwordsWithPolicy);
+			string password = PasswordPhilosophy.GetPassword(passwordsWithPolicy);
+			bool passwordIsValid = PasswordPhilosophy.PasswordIsValid(passwordPolicy, password);
+
+			Assert.AreEqual(1, passwordPolicy.NumberLeastTimeCharOccurrence);
+			Assert.AreEqual(3, passwordPolicy.NumberMostTimeCharOccurrence);
+			Assert.AreEqual('b', passwordPolicy.OccurrenceCharacterReference);
+			Assert.AreEqual("cdefg", password);
+			Assert.IsFalse(passwordIsValid);
+		}
+		
+		[TestMethod]
+		public void TryBusinessRulesWithExampleThree()
+		{
+			string passwordsWithPolicy = "2-9 c: ccccccccc";
+			PasswordPolicy passwordPolicy = PasswordPhilosophy.GetPasswordPolicy(passwordsWithPolicy);
+			string password = PasswordPhilosophy.GetPassword(passwordsWithPolicy);
+			bool passwordIsValid = PasswordPhilosophy.PasswordIsValid(passwordPolicy, password);
+
+			Assert.AreEqual(2, passwordPolicy.NumberLeastTimeCharOccurrence);
+			Assert.AreEqual(9, passwordPolicy.NumberMostTimeCharOccurrence);
+			Assert.AreEqual('c', passwordPolicy.OccurrenceCharacterReference);
+			Assert.AreEqual("ccccccccc", password);
+			Assert.IsTrue(passwordIsValid);
 		}
 	}
 }
